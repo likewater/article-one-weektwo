4ac250e2acb122d9bee5112bab40a569087b83b3
likewater <lakebayomackway@gmail.com>
2017-05-26 15:32:50 -0700

    add intent, connect button to text field

--- app/src/main/java/com/likewater/articleone/MainActivity.java
+++ app/src/main/java/com/likewater/articleone/MainActivity.java
@@ -1,13 +1,36 @@
 package com.likewater.articleone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRepsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRepsButton = (Button) findViewById(R.id.findRepsButton);
+        mFindRepsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, RepListActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+
+                }
+            });
     }
 }
--- app/src/main/java/com/likewater/articleone/RepListActivity.java
+++ app/src/main/java/com/likewater/articleone/RepListActivity.java
@@ -1,13 +1,20 @@
 package com.likewater.articleone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RepListActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rep_list);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the reps near: " + location);
     }
 }
