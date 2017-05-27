493a770d6ec09cc5f3ebedd022d19b8f3130da90
likewater <lakebayomackway@gmail.com>
2017-05-26 23:10:14 -0700

    add list view and adapter

--- app/src/main/java/com/likewater/articleone/MainActivity.java
+++ app/src/main/java/com/likewater/articleone/MainActivity.java
@@ -7,17 +7,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.graphics.Typeface;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRepsButton;
     private EditText mLocationEditText;
+    private TextView mArticleOneTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mArticleOneTextView = (TextView) findViewById(R.id.articleOneTextView);
+        Typeface cooperHewitt = Typeface.createFromAsset(getAssets(),
+                "fonts/cooperhewitt-medium.otf");
+        mArticleOneTextView.setTypeface(cooperHewitt);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRepsButton = (Button) findViewById(R.id.findRepsButton);
         mFindRepsButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/likewater/articleone/RepListActivity.java
+++ app/src/main/java/com/likewater/articleone/RepListActivity.java
@@ -3,16 +3,43 @@ package com.likewater.articleone;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RepListActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] reps = new String[] {
+           "Ron Wyden", "JeffMerkley", "Suzanne Bonamici", "Greg Walden",
+            "Earl Blumenaur", "Peter DeFazio", "Kurt Schrader"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rep_list);
+        mListView = (ListView) findViewById(R.id.repListView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                reps);
+        mListView.setAdapter(adapter);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String rep = ((TextView) view).getText().toString();
+
+                Toast.makeText(RepListActivity.this, rep, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the reps near: " + location);
